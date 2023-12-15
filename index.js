f326c23a1c0e7555fd6656a3747c59f43fb9b160
Eleni <elenimariex@gmail.com>
2023-12-15 00:15:02 +0000

    Creating questions for title, description and table of contents

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'How would you describe your project?',
+  },
+  {
+    type: 'rawList',
+    name: 'contents-table',
+    message: 'Please provide a table of contents',
+    choices: ['Title', 'Description', "Table of Contents", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"]
+  }
 ];
 
 // function to write README file
