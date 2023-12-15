8f18870f086de0c55ed3727c8b1d26299a8c0609
Eleni <elenimariex@gmail.com>
2023-12-15 00:43:21 +0000

    Adding all questions for users

--- index.js
+++ index.js
@@ -17,9 +17,44 @@ const questions = [
   },
   {
     type: 'rawList',
-    name: 'contents-table',
+    name: 'contents',
     message: 'Please provide a table of contents',
-    choices: ['Title', 'Description', "Table of Contents", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"]
+    choices: []
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How do users install your project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide instructions for using your project',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'Please select a license for your project',
+  },
+  {
+    type: 'input',
+    name: 'contributions',
+    message: 'Please include guidelines for other developers to contribute to your project',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Please provide any tests for your project',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Please provide your GitHub username',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Please provide your email',
   }
 ];
 
