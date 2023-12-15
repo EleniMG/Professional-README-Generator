b750b436a3d143d9655e04964b44ea2066ee2fd7
Eleni <elenimariex@gmail.com>
2023-12-15 12:27:44 +0000

    Using GitHub licenses npm package and spread operator to allow user to select from list of GitHub licenses

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
+const licenses = require("github-licenses")
 const generateMarkdown = require("./utils/generateMarkdown");
+
+const licensesArray = Object.keys(licenses)
 
 // array of questions for user
 const questions = [
   {
     type: 'input',
     name: 'title',
-    message: 'What is the title of your project?',
+    message: 'What is the title of your project?'
   },
   {
     type: 'input',
     name: 'description',
-    message: 'How would you describe your project?',
+    message: 'How would you describe your project?'
   },
   {
-    type: 'rawList',
+    type: 'confirm',
     name: 'contents',
-    message: 'Please provide a table of contents',
-    choices: []
+    message: 'Do you want to include a table of contents?'
   },
   {
     type: 'input',
     name: 'installation',
-    message: 'How do users install your project?',
+    message: 'How do users install your project?'
   },
   {
     type: 'input',
     name: 'usage',
-    message: 'Please provide instructions for using your project',
+    message: 'Please provide instructions for using your project'
   },
   {
-    type: 'input',
+    type: 'list',
     name: 'license',
     message: 'Please select a license for your project',
+    choices: ["None", ...licensesArray]
   },
   {
     type: 'input',
     name: 'contributions',
-    message: 'Please include guidelines for other developers to contribute to your project',
+    message: 'Please include guidelines for other developers to contribute to your project'
   },
   {
     type: 'input',
     name: 'tests',
-    message: 'Please provide any tests for your project',
+    message: 'Please provide any tests for your project'
   },
   {
     type: 'input',
     name: 'github',
-    message: 'Please provide your GitHub username',
+    message: 'Please provide your GitHub username'
   },
   {
     type: 'input',
     name: 'email',
-    message: 'Please provide your email',
+    message: 'Please provide your email'
   }
 ];
 
